f808d03ff401c6a0ec1b3e4b6f9796da0ed8013a
arllainm <arllain@gmail.com>
2020-02-25 10:08:55 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, View, Text, StatusBar } from 'react-native';
+
+function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.Container}>
+        <View>
+          <Text style={styles.Title}>Welcome to React-Native</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  Title: {
+    alignContent: 'center',
+  },
+});
+
+export default App;
